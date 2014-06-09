d0b36244736e0b1ec0fed1b182065590f2e92718
Jeffrey Edwin Seifried <jeffrey.seifried@gmail.com>
2014-06-08 17:21:58 -0700

    Problem 40 chmod +x *.py

--- src/p040.py
+++ src/p040.py
@@ -6,7 +6,8 @@
 # If dn represents the nth digit of the fractional part, find the value of the following expression.
 #   d1 x d10 x d100 x d1000 x d10000 x d100000 x d1000000
 ###
-from euler import * ;
+from euler import *
 ###
 if __name__ == '__main__':
-    pass;
+    digits = (1, 1, 5, 3, 7, 2, 1)
+    print(reduce(lambda n, m: n * m, digits))
