2d8bb91000a2dfcefad8ac8f8d348083958d708f
Jeffrey Edwin Seifried <jeffrey.seifried@gmail.com>
2014-09-14 17:49:52 -0700

    Added prompts for #041-#050

---
+++ src/p041.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+# We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+# What is the largest n-digit pandigital prime that exists?
+
+from euler import *
+
+if __name__ == '__main__':
+    pass
---
+++ src/p048.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+# The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+# Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+
+from euler import *
+
+if __name__ == '__main__':
+    pass
